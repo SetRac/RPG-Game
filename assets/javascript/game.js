4f8788cdda2c97539c55902c84b342a0b074b28c
mmusil2 <mmusil2@kent.edu>
2018-11-14 16:52:09 -0500

    added functional character and enemy select

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,7 @@
 var scorpion = {
-    name: "SCORPION", 
-    src: "assets/images/scorpion.jpg",
+    // name: "SCORPION", 
+    // src: "assets/images/scorpion.jpg",
     hp: 120,
-    ap: 8,
-    cap: 150
 };
 
 var subzero = {
@@ -17,6 +15,15 @@ var kitana = {
 var shaokahn = {
     hp: 200
 };
+
+charname = "";
+charhp = 0;
+charap = 0;
+enemyhp = 0
+enemycap = 0;
+
+charselected = false;
+enemyselected = false;
 
 $("#scorpion-hp").text(scorpion.hp);
 $("#subzero-hp").text(subzero.hp);
@@ -41,7 +48,54 @@ $("#shaokahn-hp").text(shaokahn.hp);
 
 // $("#characters").append(subzero);
 
+// $(".choice").on("click", function() {
+//     $(this).attr("src", "assets/images/kitana.jpg");
+//     $("#selected").append($(this));
+// });
+
+
+
+// if booleans are true - can't select stuff
+
+
+
 $(".choice").on("click", function() {
-    $(this).attr("src", "assets/images/kitana.jpg");
-    $("#selected").append($(this));
+    if (charselected == false && enemyselected == false) {
+        charname = $(this).attr("name");
+        charhp = $(this).attr("hp");
+        charap = $(this).attr("ap");
+        charhp = parseInt(charhp);
+        charap = parseInt(charap);
+        $("#selected").append($(this));
+        if (charname == "scorpion") {
+            $("#enemy").append($("#sub"));
+            $("#enemy").append($("#kit"));
+            $("#enemy").append($("#shao"));
+        }
+        if (charname == "subzero") {
+            $("#enemy").append($("#scor"));
+            $("#enemy").append($("#kit"));
+            $("#enemy").append($("#shao"));
+        }
+        if (charname == "kitana") {
+            $("#enemy").append($("#scor"));
+            $("#enemy").append($("#sub"));
+            $("#enemy").append($("#shao"));
+        }
+        if (charname == "shaokahn") {
+            $("#enemy").append($("#scor"));
+            $("#enemy").append($("#kit"));
+            $("#enemy").append($("#sub"));
+        }
+        charselected = true;
+    } else if (charselected == true && enemyselected == false) {
+        enemyhp = $(this).attr("hp");
+        enemycap = $(this).attr("cap");
+        enemyhp = parseInt(enemyhp);
+        enemycap = parseInt(enemycap);
+        $("#defender").append($(this));
+        enemyselected = false;
+    }
 });
+
+console.log(charhp);
